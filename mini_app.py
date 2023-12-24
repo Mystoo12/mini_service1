5484c4fc00ca3ed9f8b889b3f30d2ddf462a1375
unknown <amir.benjjam@gmail.com>
2023-12-24 12:36:46 +0100

    fichier mini_app

---
+++ mini_app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def generer_nombre_aleatoire():
+    nombre_aleatoire = random.randint(1,100)
+    return render_template('index.html', nombre_aleatoire = nombre_aleatoire)
+
+if __name__ == '__main__':
+    app.run(debug=True, host = '0.0.0.0')
+
