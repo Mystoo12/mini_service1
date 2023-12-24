27c6bfa573f0c86839ed308dfc13a297c7970be8
unknown <amir.benjjam@gmail.com>
2023-12-24 13:55:47 +0100

    commit final

--- mini_app.py
+++ mini_app.py
@@ -10,5 +10,5 @@ def generer_nombre_aleatoire():
     return render_template('index.html', nombre_aleatoire = nombre_aleatoire)
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
